f6acbeaf8116e25904fb6a9e99d7010661cf5ddb
Gertt <gert@gert.gert>
2016-01-24 19:35:55 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import parse from './jwt';
+
+const jwt = parse(window.location.hash);
+
+if (jwt && jwt.state) {
+    const target = jwt.state;
+
+    // Use target protocol or fallback to current protocol
+    let dest = target.protocol || window.location.protocol;
+
+    // Get our destination from JWT state, can't do fallback to current here
+    dest += '//' + target.hostname + target.pathname + target.search;
+
+    // Use current hash for destination (it has the auth0 response)
+    dest += window.location.hash;
+
+    // Go there
+    window.location.href = dest;
+}
+
+throw 'Failed to parse hash as an Auth0 response.';
